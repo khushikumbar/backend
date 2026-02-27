3a1098377912f7c549911addcf07a5ffad48a96b
khushikumbar <kumbarkhushi38@gmail.com>
2026-02-27 17:48:33 +0530

    Fix syntax error in CORS origin URL

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: https://frontend-eabe.vercel.app",
+  origin: "https://frontend-eabe.vercel.app",
   credentials: true,
 }));
 
